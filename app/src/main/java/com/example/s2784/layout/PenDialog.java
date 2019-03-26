a8b28afa375bfd23c65be6fa4d13a57e6795cff1
adam1214 <34540871+adam1214@users.noreply.github.com>
2019-03-26 16:34:39 +0800

    Use Snackbar as feeback that tells user to key in words in discussion room
    1. Original code is commented.
    2. Have problem that can't into file brower(will crashed,I commented that code).I don't know the reason.
    3. Have problem that when touch the floatingActionButton, the screen will turn gray until user touch the screen.
    4. Have problem that user touch the zone that is not in the dialog,dialog will dismiss.

--- app/src/main/java/com/example/s2784/layout/PenDialog.java
+++ app/src/main/java/com/example/s2784/layout/PenDialog.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +16,8 @@ public class PenDialog extends AppCompatDialogFragment {
 
     private EditText et_content;
     private PenDialogListener penDialogListener;
-    private static Toast toast;
+
+    AlertDialog dialogDemo;
 
     @Override
     public void onAttach(Context context) {
@@ -28,56 +30,107 @@ public class PenDialog extends AppCompatDialogFragment {
         }
     }
 
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
+//    @Override
+//    public Dialog onCreateDialog(Bundle savedInstanceState) {
+//
+//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//
+//        LayoutInflater inflater = getActivity().getLayoutInflater();
+//        View view = inflater.inflate(R.layout.dialog_pen,null);
+//        builder.setView(view)
+//                .setTitle("回覆")
+//                .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        String content = et_content.getText().toString();
+//                        if (content != null && !content.equals(""))
+//                        {
+//                            penDialogListener.applyTexts(content);
+//                        }
+//                        else
+//                        {
+//                            Context context = getActivity().getApplicationContext();
+//                            makeTextAndShow(context, "沒有回覆訊息，請重新輸入", Toast.LENGTH_SHORT);
+//                        }
+//                    }
+//                })
+//                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//
+//                    }
+//                });
+//
+//        et_content = view.findViewById(R.id.pen_et_content);
+//        return builder.create();
+//    }
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AlertDialog.Builder dialogBuild = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.dialog_pen,null);
-        builder.setView(view)
-                .setTitle("回覆")
-                .setPositiveButton("確定", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String content = et_content.getText().toString();
-                        if (content != null && !content.equals(""))
-                        {
-                            penDialogListener.applyTexts(content);
-                        }
-                        else
-                        {
-                            Context context = getActivity().getApplicationContext();
-                            makeTextAndShow(context, "沒有回覆訊息，請重新輸入", Toast.LENGTH_SHORT);
-                        }
-                    }
-                })
-                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+        final View view = inflater.inflate(R.layout.dialog_pen,null);
+        dialogBuild.setTitle("回覆");
+        dialogBuild.setView(view);
+        dialogBuild.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String content = et_content.getText().toString();
+                if (content != null && !content.equals("")) {
+                    penDialogListener.applyTexts(content);
+                    //該條件下關閉dialog
+                    closeDialog(dialogDemo);
+                    //此句最好不要省略，否則在有些情況下dialog不關閉
+                    dialog.dismiss();
 
-                    }
-                });
+                } else {
+                    //該條件下不關閉dialog
+                    keepDialogOpen(dialogDemo);
+                    Snackbar.make(view, "請輸入回覆訊息", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        dialogBuild.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //取消按鈕在任何條件下都關閉
+                closeDialog(dialogDemo);
+                //此句最好不要省略，否則在有些情況下dialog不關閉
+                dialog.dismiss();
+            }
+        });
 
         et_content = view.findViewById(R.id.pen_et_content);
-        return builder.create();
+        dialogDemo = dialogBuild.create();
+        dialogDemo.show();
+
     }
 
     public interface PenDialogListener{
         void applyTexts(String content);
     }
-    private static void makeTextAndShow(final Context context, final String text, final int duration)
-    {
-        if (toast == null)
-        {
-            //如果還沒有用過makeText方法，才使用
-            toast = android.widget.Toast.makeText(context, text, duration);
+
+    //保持dialog不關閉的方法
+    private void keepDialogOpen(AlertDialog dialog) {
+        try {
+            java.lang.reflect.Field field = dialog.getClass().getSuperclass().getDeclaredField("mShowing");
+            field.setAccessible(true);
+            field.set(dialog, false);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        else
-        {
-            toast.setText(text);
-            toast.setDuration(duration);
+    }
+
+    //關閉dialog的方法
+    private void closeDialog(AlertDialog dialog) {
+        try {
+            java.lang.reflect.Field field = dialog.getClass().getSuperclass().getDeclaredField("mShowing");
+            field.setAccessible(true);
+            field.set(dialog, true);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        toast.show();
     }
 }
--- app/src/main/java/com/example/s2784/layout/PlusDialog.java
+++ app/src/main/java/com/example/s2784/layout/PlusDialog.java
@@ -1,23 +1,26 @@
 package com.example.s2784.layout;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class PlusDialog extends AppCompatDialogFragment {
 
     private EditText et_title;
     private EditText et_content;
     private PlusDialogListener plusDialogListener;
-    private static Toast toast;
+
+    AlertDialog dialogDemo;
 
     @Override
     public void onAttach(Context context) {
@@ -30,66 +33,128 @@ public class PlusDialog extends AppCompatDialogFragment {
         }
     }
 
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//    @Override
+//    public Dialog onCreateDialog(Bundle savedInstanceState) {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//
+//        LayoutInflater inflater = getActivity().getLayoutInflater();
+//        View view = inflater.inflate(R.layout.dialog_plus,null);
+//        builder.setView(view)
+//                .setTitle("發佈新主題")
+//                .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        String title = et_title.getText().toString();
+//                        String content = et_content.getText().toString();
+//                        if (title != null && !title.equals("") && content != null && !content.equals(""))
+//                        {
+//                            plusDialogListener.applyTexts(title, content);
+//                        }
+//                        else
+//                        {
+//                            Context context = getActivity().getApplicationContext();
+//                            makeTextAndShow(context, "沒有主題或內容，請重新輸入", Toast.LENGTH_SHORT);
+//                        }
+//                    }
+//                })
+//                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//
+//                    }
+//                })
+//                .setNeutralButton("選擇檔案", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        Intent picker = new Intent(Intent.ACTION_GET_CONTENT);
+//                        picker.setType("*/*");
+//                        startActivity(picker);
+//                    }
+//                });
+//
+//
+//        et_title = view.findViewById(R.id.plus_et_title);
+//        et_content = view.findViewById(R.id.plus_et_content);
+//        return builder.create();
+//    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AlertDialog.Builder dialogBuild = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.dialog_plus,null);
-        builder.setView(view)
-                .setTitle("發佈新主題")
-                .setPositiveButton("確定", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String title = et_title.getText().toString();
-                        String content = et_content.getText().toString();
-                        if (title != null && !title.equals("") && content != null && !content.equals(""))
-                        {
-                            plusDialogListener.applyTexts(title, content);
-                        }
-                        else
-                        {
-                            Context context = getActivity().getApplicationContext();
-                            makeTextAndShow(context, "沒有主題或內容，請重新輸入", Toast.LENGTH_SHORT);
-                        }
-                    }
-                })
-                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+        final View view = inflater.inflate(R.layout.dialog_plus,null);
+        dialogBuild.setTitle("發佈新主題");
+        dialogBuild.setView(view);
+        dialogBuild.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String title = et_title.getText().toString();
+                String content = et_content.getText().toString();
+                if (title != null && !title.equals("") && content != null && !content.equals("")) {
+                    plusDialogListener.applyTexts(title, content);
+                    //該條件下關閉dialog
+                    closeDialog(dialogDemo);
+                    //此句最好不要省略，否則在有些情況下dialog不關閉
+                    dialog.dismiss();
 
-                    }
-                })
-                .setNeutralButton("選擇檔案", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Intent picker = new Intent(Intent.ACTION_GET_CONTENT);
-                        picker.setType("*/*");
-                        startActivity(picker);
-                    }
-                });
+                } else {
+                    //該條件下不關閉dialog
+                    keepDialogOpen(dialogDemo);
+                    Snackbar.make(view, "沒有主題或內容，請重新輸入", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
 
+        dialogBuild.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //取消按鈕在任何條件下都關閉
+                closeDialog(dialogDemo);
+                //此句最好不要省略，否則在有些情況下dialog不關閉
+                dialog.dismiss();
+            }
+        });
 
+        dialogBuild.setNeutralButton("選擇檔案", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //this will crash the app
+//                Intent picker = new Intent(Intent.ACTION_GET_CONTENT);
+//                picker.setType("*/*");
+//                startActivity(picker);
+            }
+        });
         et_title = view.findViewById(R.id.plus_et_title);
         et_content = view.findViewById(R.id.plus_et_content);
-        return builder.create();
+        dialogDemo = dialogBuild.create();
+        dialogDemo.show();
+
     }
 
     public interface PlusDialogListener{
         void applyTexts(String title, String content);
     }
-    private static void makeTextAndShow(final Context context, final String text, final int duration)
-    {
-        if (toast == null)
-        {
-            //如果還沒有用過makeText方法，才使用
-            toast = android.widget.Toast.makeText(context, text, duration);
+
+    //保持dialog不關閉的方法
+    private void keepDialogOpen(AlertDialog dialog) {
+        try {
+            java.lang.reflect.Field field = dialog.getClass().getSuperclass().getDeclaredField("mShowing");
+            field.setAccessible(true);
+            field.set(dialog, false);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        else
-        {
-            toast.setText(text);
-            toast.setDuration(duration);
+    }
+
+    //關閉dialog的方法
+    private void closeDialog(AlertDialog dialog) {
+        try {
+            java.lang.reflect.Field field = dialog.getClass().getSuperclass().getDeclaredField("mShowing");
+            field.setAccessible(true);
+            field.set(dialog, true);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        toast.show();
     }
 }
